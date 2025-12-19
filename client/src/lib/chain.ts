1b87bdeba030e2c2d6009ccdbb87a56bca1a09e2
cakezero <sareahj.com@gmail.com>
2025-12-19 01:32:56 +0100

    claim trust tokens from contract

---
+++ client/src/lib/chain.ts
@@ -0,0 +1,41 @@
+import { defineChain } from "viem";
+
+const intuitionMainnet = defineChain({
+  id: 1155,
+  name: "Intuition Mainnet",
+  nativeCurrency: {
+    decimals: 18,
+    name: "TRUST",
+    symbol: "TRUST",
+  },
+  rpcUrls: {
+    default: {
+      http: ["https://rpc.intuition.systems"],
+    },
+  },
+  blockExplorers: {
+    default: { name: "Explorer", url: "https://explorer.intuition.systems" },
+  },
+});
+
+const intuitionTestnet = defineChain({
+  id: 13579,
+  name: "Intuition Testnet",
+  nativeCurrency: {
+    decimals: 18,
+    name: "tTRUST",
+    symbol: "tTRUST",
+  },
+  rpcUrls: {
+    default: {
+      http: ["https://testnet.rpc.intuition.systems"],
+    },
+  },
+  blockExplorers: {
+    default: { name: "Explorer", url: "https://testnet.explorer.intuition.systems" },
+  },
+});
+
+const chain = import.meta.env.network === "mainnet" ? intuitionMainnet : intuitionTestnet;
+
+export default chain;
