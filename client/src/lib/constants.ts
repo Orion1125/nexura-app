847c9aec76cc3138fad4e4b6e01d072358063c79
cakezero <sareahj.com@gmail.com>
2025-12-27 23:10:46 +0100

    oauth url set for DC

--- client/src/lib/constants.ts
+++ client/src/lib/constants.ts
@@ -6,3 +6,13 @@ export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 export const url = import.meta.env.VITE_CLIENT_URL || "https://nexura-app.vercel.app";
 
 export const projectId = import.meta.env.VITE_REOWN_PROJECT_ID;
+
+export const discordAuthUrl =
+	import.meta.env.VITE_ENV === "development"
+		? "https://discord.com/oauth2/authorize?client_id=1452214561238286419&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A5173%2Fdiscord%2Fcallback&scope=identify"
+		: "https://discord.com/oauth2/authorize?client_id=1452214561238286419&response_type=code&redirect_uri=https%3A%2F%2Fnexura-app.vercel.app%2Fdiscord%2Fcallback&scope=identify";
+
+export const twitterAuthUrl =
+	import.meta.env.VITE_ENV === "development"
+		? ""
+    : "";
